3bd68de5d28b6fdc9f850151777fd319aade10bf
Pedro Augusto Guimarães Santos <pedroaugustoguimaraesantos@gmail.com>
2026-03-23 11:27:20 -0300

    add initial structure to recipe website

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from recipes.views import home, about, contact
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
-    path('about/', about),
-    path('contact/', contact),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-def about(request):
-    return HttpResponse('project about')
-
-def contact(request):
-    return HttpResponse('project contact')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio'
+    })
