d3a0f0abc1978d5640a8a8dff1eed99838b0b5e3
Pedro Augusto Guimarães Santos <pedroaugustoguimaraesantos@gmail.com>
2026-03-25 11:21:02 -0300

    refactor recipe rendering and data generation for improved dynamic content display

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Luiz Otávio'
+        'recipes': [make_recipe() for _ in range(9)]
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'id': id
+        'recipe': make_recipe()
     })
